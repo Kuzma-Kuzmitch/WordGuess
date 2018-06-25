006f5c4513fa48af0bc2506783d4cccba47593fd
Evan Kuzma <evankuzma@gmail.com>
2018-06-25 16:52:53 -0400

    first stage

---
+++ assets/javascript/game.js
@@ -0,0 +1,60 @@
+var wordBank = {
+  "Films": ["Trainspotting", "Snatch", "Spiderman", "Cinderella", "Titanic"],
+  "Bands": ["Beatles", "Strokes", "Madonna", "Prince", "U2", "Pavement"],
+  "Shows": ["Westworld", "Lost", "Office", "Simpsons", "Jeopardy"],
+  "Input": [],
+}
+
+// var computer = words[Math.floor(words.length * Math.random())]
+
+
+
+
+
+
+
+
+
+function example(arr) {
+  var pic = wordBank.arr[Math.floor(wordBank.arr.length * Math.random())]
+  console.log(pic)
+  console.log(pic.length)
+  letters.textContent = pic.length
+  // currentWord.textContent =
+
+
+};
+
+// function shows() {
+//   var pic = wordBank.Shows[Math.floor(wordBank.Shows.length * Math.random())]
+//   console.log(pic)
+//   console.log(pic.length)
+//   document.getElementById("letters").textContent = pic.length
+//
+// };
+//
+// function films() {
+//   var pic = wordBank.Films[Math.floor(wordBank.Films.length * Math.random())]
+//   console.log(pic)
+//   console.log(pic.length)
+//   document.getElementById("letters").textContent = pic.length
+//
+// };
+//
+// function input() {
+//   var user = prompt("Add a word")
+//   wordBank.Input.push(user)
+//   var pic = wordBank.Input[Math.floor(wordBank.Input.length * Math.random())]
+//   console.log(pic)
+//   console.log(pic.length)
+//   document.getElementById("gameBox").textContent = pic + " " + pic.length};
+//
+
+
+  //
+  // document.addEventListener("keydown", function(event) {
+  //   if (event.key == "h") {
+  //     console.log(wordBank.Films[Math.floor(wordBank.Films.length * Math.random())])
+  //
+  //   }
+  // });
